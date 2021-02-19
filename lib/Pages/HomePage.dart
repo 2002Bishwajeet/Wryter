0328bbc4c2ed3746567fbdd4833029f9ab8f977e
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-02-19 16:56:12 +0530

    basic layout

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -7,32 +7,34 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int _currentindex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-       body: Center(child: Text('Tap the + icon to add Notes')),
-       floatingActionButton: FloatingActionButton(
-         backgroundColor: Colors.blue[900],
+    return Scaffold(
+      body: Center(child: Text('Tap the + icon to add Notes')),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue[900],
         onPressed: () => setState(() => null),
-        child: const Icon(Icons.add,
-        color: Colors.white,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
-
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-             BottomNavigationBarItem(
-              icon: Icon(Icons.mail),
-              label: 'Feedback'
-              ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.people),
-              label: 'About'
-              ),
-              
-          ],
-        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentindex,
+        // type: BottomNavigationBarType.fixed,
+       // iconSize: 30,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Feedback'), //backgroundcolor: Colors.yourchoice  // must use it in futures
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'About'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentindex = index;
+          });
+        },
+      ),
     );
   }
 }
